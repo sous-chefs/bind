41f16710b2834266c7db8da7088be9ece5bcf9a4
Eric G. Wolfe <wolfe21@marshall.edu>
2012-06-06 18:03:25 -0400

    remove somewhat private zone files

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "wolfe21@marshall.edu"
 license          "Apache 2.0"
 description      "Installs/Configures dns"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.4"
+version          "0.0.5"
 name             "bind"
 %w{ redhat centos scientific amazon ubuntu debian }.each do |os|
   supports os
