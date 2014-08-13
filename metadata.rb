afd75314e885bed7b82f5ee87a5f9ae3013a5c1b
Patrick Moore <moore267@marshall.edu>
2014-08-13 15:20:40 -0400

    fixed rubocop

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email 'wolfe21@marshall.edu'
 license 'Apache 2.0'
 description 'Installs/Configures ISC BIND'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
+version IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 name 'bind'
