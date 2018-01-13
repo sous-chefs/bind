81185844b31a559b55f82b423338dc7d429fbe8c
David Bruce <djb@ragnarok.net>
2018-01-13 14:28:44 +0100

    Add views kitchen test

---
+++ test/fixtures/cookbooks/bind_test/recipes/views.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+#
+bind_service 'default' do
+  action [:create, :start]
+end
+
+bind_config 'default' do
+  default_view 'internal'
+end
+
+bind_view 'internal' do
+  match_clients ['10.0.0.0/8']
+  options ['recursion yes']
+end
+
+bind_primary_zone 'example.com'
+
+bind_secondary_zone 'secondary.example.com' do
+  primaries ['1.1.1.1', '1.1.1.2']
+end
+
+bind_forward_zone 'forward.example.com' do
+  forwarders ['1.1.1.1', '1.1.1.2']
+end
+
+bind_primary_zone_template 'internal-sub.example.com' do
+  zone_name 'sub.example.com'
+  records [
+    { owner: 'www', type: 'A', ttl: 100, rdata: '10.2.1.1' },
+    { owner: 'www', type: 'A', ttl: 100, rdata: '10.1.1.1' },
+    { owner: 'www2', type: 'A', ttl: 100, rdata: '10.2.1.1' },
+    { owner: 'www2', type: 'A', ttl: 100, rdata: '10.1.1.1' },
+    { owner: 'www2', type: 'A', ttl: 100, rdata: '10.4.1.1' },
+    { owner: 'www', type: 'A', ttl: 100, rdata: '10.4.1.1' },
+    { type: 'MX', rdata: '20 mx2.example.com.' },
+    { type: 'MX', rdata: '10 mx1.example.com.' },
+    { type: 'NS', rdata: 'ns1.example.com.' },
+    { type: 'NS', rdata: 'ns0.example.com.' },
+    { type: 'NS', rdata: 'ns3.example.com.' },
+    { type: 'A', rdata: '10.0.0.1' },
+  ]
+
+  soa serial: '5'
+end
+
+bind_view 'external' do
+  options ['recursion no']
+end
+
+bind_primary_zone_template 'external-sub.example.com' do
+  view 'external'
+  zone_name 'sub.example.com'
+  records [
+    { owner: 'www', type: 'A', ttl: 100, rdata: '192.168.1.1' },
+    { type: 'MX', rdata: '20 mx2.example.com.' },
+    { type: 'MX', rdata: '10 mx1.example.com.' },
+    { type: 'NS', rdata: 'ns1.example.com.' },
+    { type: 'NS', rdata: 'ns0.example.com.' },
+    { type: 'NS', rdata: 'ns3.example.com.' },
+    { type: 'A', rdata: '192.168.1.2' },
+  ]
+
+  soa serial: '10'
+end
