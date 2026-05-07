d5cf9e508e82e471eb88454a7b88d4ff9f206fd6
Dan Webb <dan.webb@damacus.io>
2026-05-07 23:13:20 +0100

    fix: mask resolved during integration tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ RSpec.configure(&:raise_errors_for_deprecations!)
 
 shared_context 'version_stub' do
   before do
+    stub_command('systemctl is-enabled systemd-resolved.service 2>/dev/null | grep -q "^masked$"').and_return(false)
+
     stubs_for_provider('bind_config[default]') do |provider|
       allow(provider).to receive_shell_out('named -v', stdout: 'BIND 9.16.23 (Extended Support Version) <id:fde3b1f>')
     end
